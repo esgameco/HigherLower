016aaf1196d66ec8d7420f74a0939bb5e3dcf475
Chance Vodnoy <esgameco@gmail.com>
2021-06-13 00:58:27 -0700

    Added ui and fixed build

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,12 +1,104 @@
+//
+// Constructor
+//
+
 window.addEventListener('load', async () => {
     // Injects Web3 into website
     window.ethereum.request({method: 'eth_requestAccounts'});
     window.web3 = new Web3(window.ethereum);
-
     window.contract = await getContract();
-    await window.contract.methods.startNextGame().send({'from':'0x3Bc0b65ea00a69bb0315ab9b16431638d086D731'})
-    await window.contract.methods.bet().call({'value':10000000});
+    window.address = '0x3Bc0b65ea00a69bb0315ab9b16431638d086D731'; // web3.eth.accounts[0] is equivalent for the future
+
+    // Initializes info
+    updateInfo();
+
+    // Buttons
+    const newGameButton = document.getElementById('hl-btn-new-game');
+    const betButton = document.getElementById('hl-btn-bet');
+    const claimButton = document.getElementById('hl-btn-claim');
+    const updateInfoButton = document.getElementById('hl-btn-update-info');
+
+    // Listeners
+    newGameButton.addEventListener('click', newGame);
+    // get label element value and pass to bet to change the value of the bet
+    betButton.addEventListener('click', bet);
+    claimButton.addEventListener('click', claim);
+    updateInfoButton.addEventListener('click', updateInfo);
 });
+
+//
+// View functions:
+//
+
+// Updates info in card
+const updateInfo = async () => {
+    const currentGameInfo = document.getElementById('hl-info-game');
+    const betTotalInfo = document.getElementById('hl-info-bet-total');
+    const betInfo = document.getElementById('hl-info-bet');
+    const claimableInfo = document.getElementById('hl-info-claimable');
+    const newGameInfo = document.getElementById('hl-info-start-new');
+
+    currentGameInfo.innerHTML = await getCurrentGame();
+    betTotalInfo.innerHTML = String(await getBetTotal()) + ' eth';
+    betInfo.innerHTML = String(await getBet()) + ' eth';
+    claimableInfo.innerHTML = await isClaimable();
+    newGameInfo.innerHTML = await newGameAvailable();
+};
+
+// Gets current game
+const getCurrentGame = async () => {
+    return await window.contract.methods.currentGame().call({'from': window.address});
+};
+
+// Gets bet total for current game
+const getBetTotal = async () => {
+    const total = await window.contract.methods.getCurrentTotal().call({'from': window.address});
+    return window.web3.utils.fromWei(total);
+};
+
+// Gets Bet for an Address
+const getBet = async () => {
+    const bet = await window.contract.methods.getCurrentBet().call({'from': window.address});
+    return window.web3.utils.fromWei(bet);
+};
+
+// Checks whether the prize is claimable
+const isClaimable = async () => {
+    return await window.contract.methods.getClaimable().call({'from': window.address});
+};
+
+// Checks whether new game is available
+const newGameAvailable = async () => {
+    return await window.contract.methods.getNewGame().call({'from': window.address});
+};
+
+//
+// Transaction Functions:
+//
+
+// Bets amount from account
+const bet = async () => {
+    return await window.contract.methods.bet().send({'from': window.address, 'value': 10000000000000});
+};
+
+// Withdraws amount from account
+const withdraw = async () => {
+    await window.contract.methods.withdraw(amount).send({'from': window.address});
+};
+
+// Claim rewards
+const claim = async () => {
+    await window.contract.methods.claim().send({'from': window.address});
+};
+
+// Starts the next game
+const newGame = async () => {
+    return await window.contract.methods.startNextGame().send({'from': window.address});
+};
+
+//
+// Contract
+//
 
 const getContract = async () => {
     return await new window.web3.eth.Contract([
@@ -386,5 +478,5 @@ const getContract = async () => {
             "type": "function",
             "constant": true
           }
-        ], '0x6d6F997b76272fC27C908147DEFc1a42a877fA2d');
+        ], '0xF31b5f86E0885fD01D3012fd31c8D32b4A01E795');
 };
